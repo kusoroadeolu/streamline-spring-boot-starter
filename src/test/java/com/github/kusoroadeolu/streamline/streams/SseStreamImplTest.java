9174098cbb9b8ff560fb6bc2bb222795ce7a4d10
Kusoro Adeolu <kusoroadeolu@gmail.com>
2025-12-10 03:23:59 +0100

    chore: complete unit tests for sse stream

--- src/test/java/com/github/kusoroadeolu/streamline/streams/SseStreamImplTest.java
+++ src/test/java/com/github/kusoroadeolu/streamline/streams/SseStreamImplTest.java
@@ -2,6 +2,7 @@ package com.github.kusoroadeolu.streamline.streams;
 
 import com.github.kusoroadeolu.streamline.exceptions.SseStreamCompletedException;
 import com.github.kusoroadeolu.streamline.exceptions.SseStreamIOException;
+import org.apache.tomcat.util.threads.VirtualThreadExecutor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutorService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,6 +26,7 @@ class SseStreamImplTest {
     private SseStreamImplBuilder errorSseStreamImplBuilder;
     private ErrorEmitter errorEmitter;
     private SseStream errorStream;
+
 
     @BeforeEach
     public void onSetup(){
@@ -81,15 +84,18 @@ class SseStreamImplTest {
 
     @Test
     void shouldRunAsync_onSend(){
+        SlowEmitter sm = new SlowEmitter();
+        SseStream slowStream = new SseStreamImplBuilder(sm).build();
+
         int id = 1;
         String name = "event";
         String content = "content";
         TestEvent testEvent = new TestEvent(id, name, content);
-        CompletableFuture<Void> v = stream.send(testEvent);
+        CompletableFuture<Void> v = slowStream.send(testEvent);
 
         assertFalse(v.isDone());
-        assertTrue(emitter.senderThread.isVirtual());
-        assertNotEquals(Thread.currentThread(), emitter.senderThread);
+        assertTrue(sm.senderThread.isVirtual());
+        assertNotEquals(Thread.currentThread(), sm.senderThread);
     }
 
     @Test
@@ -113,22 +119,84 @@ class SseStreamImplTest {
         TestEvent testEvent = new TestEvent(id, name, content);
         stream.send(testEvent).join();
         stream.complete();
-
         assertThrows(SseStreamCompletedException.class, () -> stream.send(testEvent));
     }
+
+    @Test
+    void shouldHandleConcurrentSends(){
+        String name = "event";
+        String content = "content";
+        ArrayList<CompletableFuture<Void>> futures = new ArrayList<>();
+        try(ExecutorService executor = new VirtualThreadExecutor("")){
+            for (int i = 0; i < 1000; i++){
+                TestEvent testEvent = new TestEvent(i, name, content);
+                futures.add(CompletableFuture.runAsync(() -> stream.send(testEvent), executor));
+
+            }
+            CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new)).join();
+        }
+
+        assertEquals(1000, emitter.sent.size());
+    }
+
+    @Test
+    void shouldNotSendAfterComplete_underConcurrentSends(){
+        String name = "event";
+        String content = "content";
+        ArrayList<CompletableFuture<Void>> futures = new ArrayList<>();
+        try(ExecutorService executor = new VirtualThreadExecutor("")){
+            for (int i = 0; i < 100; i++){
+                TestEvent testEvent = new TestEvent(i, name, content);
+                futures.add(CompletableFuture.runAsync(() -> stream.send(testEvent), executor));
+                if (i == 30) stream.complete();
+            }
+
+            try{
+                CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new)).join();
+            }catch (CompletionException e){
+
+            }
+        }
+
+        assertTrue(emitter.sent.size() <= 30);
+        assertTrue(emitter.sent.stream().map(TestEvent::id).allMatch(i -> i <= 30));
+    }
+
+    @Test
+    void shouldDoNothing_whenCompleteIsCalled_afterStreamIsCompleted(){
+        stream.complete();
+        assertDoesNotThrow(() -> stream.complete());
+    }
+
+    @Test
+    void shouldThrowNothing_onSlowSend(){
+        SlowEmitter sm = new SlowEmitter();
+        SseStream slowStream = new SseStreamImplBuilder(sm).build();
+
+        int id = 1;
+        String name = "event";
+        String content = "content";
+        TestEvent testEvent = new TestEvent(id, name, content);
+        var v = slowStream.send(testEvent);
+        stream.complete();
+
+        assertDoesNotThrow(v::join);
+    }
+
+
 
 }
 
 record TestEvent(int id, String name, String content){}
 
 class FakeEmitter extends SseEmitter{
-    public List<Object> sent = new ArrayList<>();
+    public List<TestEvent> sent = new ArrayList<>();
     public Thread senderThread =  null;
     public FakeEmitter() { super(Long.MAX_VALUE); }
 
     @Override
     public void send(Object obj, MediaType type) throws IOException{
-        sent.add(obj);
+        sent.add((TestEvent) obj);
         senderThread = Thread.currentThread();
     }
 }
@@ -137,5 +205,17 @@ class ErrorEmitter extends FakeEmitter{
     @Override
     public void send(Object obj, MediaType type) throws IOException{
         throw new IOException("fail");
+    }
+}
+
+class SlowEmitter extends FakeEmitter{
+    public Thread senderThread =  null;
+    @Override
+    public void send(Object obj, MediaType type) throws IOException{
+        senderThread = Thread.currentThread();
+        try {
+            Thread.sleep(400);
+        }catch (InterruptedException e){}
+        sent.add((TestEvent) obj);
     }
 }
